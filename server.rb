307ebb8a79d28875b70e6a9ab2e2b6e60bbfbfdf
ank13 <avi.kaufman@gmail.com>
2013-07-20 23:19:16 -0500

    Makes Post request to create a block of sayings.

--- server.rb
+++ server.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
 require './generator'
+require './helpers'
 
 get '/' do
   @saying = Sayings.yiddish_with_translation
   erb :index
 end
+
+post '/nu' do
+  # convert param keys from strings to sybmols
+  requested_block = Helpers.symbolize_keys(params)
+  # get the block of yiddish ipsum
+  @yipsum_block = Sayings.yipsum(requested_block)
+
+  if request.xhr?
+    erb :_yipsum, :layout => false
+  else
+    erb :yipsum
+  end
+
+end
+
+
+
